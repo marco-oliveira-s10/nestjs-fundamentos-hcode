07d54a1d4784d5115492173827f941d06e53d577
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:25:18 -0300

    Conceito de Decorators

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// No NestJS (e em TypeScript em geral), decorators são funções que permitem adicionar metadata ou modificar o comportamento de classes, métodos, propriedades ou parâmetros. Eles são uma característica poderosa da linguagem e são amplamente utilizados no NestJS para simplificar a criação de componentes e a configuração de rotas, serviços e injeção de dependências.
 @Module({
   imports: [],
   controllers: [AppController],
