f0f85e61372f9efb0cf3d04e1aca7ee87b4f906b
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:32:27 -0300

    Conceito de Service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+//No NestJS, Services (ou Serviços) são componentes que contêm a lógica de negócios da aplicação. Eles são responsáveis por realizar operações específicas, como acessar um banco de dados, manipular dados, executar cálculos ou interagir com APIs externas. Os serviços são frequentemente utilizados pelos controladores para processar requisições e fornecer respostas.
 @Injectable()
 export class AppService {
+  
   getHello(): string {
     return 'Hello World!';
   }
+
 }
