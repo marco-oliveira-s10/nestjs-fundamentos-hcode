6ce20e056c7b20fa680a265375de73d4408ee17a
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:50:22 -0300

    CRUD - create

---
+++ src/user/user.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+
+        return { body };
+
+    }
+}
