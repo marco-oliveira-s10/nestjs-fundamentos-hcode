192519fbca25a4b1fdbc964a31ef387ae91e6e02
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:30:32 -0300

    Conceito de Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
+//No NestJS, Controllers são responsáveis por gerenciar as rotas e manipular as requisições HTTP recebidas pela aplicação.
 @Controller()
 export class AppController {
 
