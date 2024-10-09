3c04e9ac9c614ded4aa78598b27f985d47aec0e2
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:29:42 -0300

    Conceito de Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // No NestJS (e em TypeScript em geral), decorators são funções que permitem adicionar metadata ou modificar o comportamento de classes, métodos, propriedades ou parâmetros. Eles são uma característica poderosa da linguagem e são amplamente utilizados no NestJS para simplificar a criação de componentes e a configuração de rotas, serviços e injeção de dependências.
+// Modulo é uma estrutura que agrupa componentes relacionados (controladores, serviços, etc.) em uma única unidade, facilitando a organização e a reutilização de código.
 @Module({
   imports: [],
   controllers: [AppController],
