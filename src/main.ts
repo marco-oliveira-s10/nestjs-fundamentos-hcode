3be3be3a4949003322f3efd070dc123166e0a53a
Marco Oliveira <marco.oliveira.s10@gmail.com>
2024-10-08 22:23:10 -0300

    + Conceito Bootstrap em NestJS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 // No NestJS, o termo bootstrap refere-se ao processo de inicialização da aplicação. Em um contexto mais amplo, é o momento em que a aplicação é configurada e todos os módulos, controladores e provedores são registrados e preparados para serem usados.
+// É a fase de inicialização da aplicação NestJS, onde a instância principal da aplicação é criada e configurada, permitindo que o framework comece a ouvir requisições e a interagir com os componentes definidos.
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
